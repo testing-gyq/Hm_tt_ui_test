0e08c7b5d2f0e35600e544087fcafeac5af67e46
guyanqi <guyanqi9001@outlook.com>
2020-08-28 12:28:39 +0800

    add a file

--- case/mp/test_begin.py
+++ case/mp/test_begin.py
@@ -2,9 +2,10 @@ import pytest
 
 from utils import DriverUtils
 
+
 @pytest.mark.run(order=1)
 class TestBegin:
     def test_begin(self):
-        print("order_v1.2.0")
-        #关闭浏览器驱动的开关
+        print("order_v1.2.0,你好")
+        # 关闭浏览器驱动的开关
         DriverUtils.chang_mp_key(False)
