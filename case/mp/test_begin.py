9afc2cd4220789a1eca1f1804452905b5bbc262a
guyanqi <guyanqi9001@outlook.com>
2020-08-28 11:59:57 +0800

    提交到v2

--- case/mp/test_begin.py
+++ case/mp/test_begin.py
@@ -5,5 +5,6 @@ from utils import DriverUtils
 @pytest.mark.run(order=1)
 class TestBegin:
     def test_begin(self):
+        print("order_v1.2.0")
         #关闭浏览器驱动的开关
         DriverUtils.chang_mp_key(False)
